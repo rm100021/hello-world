243859154ecfddc133071c09af38eabbe492ac27
rm100021 <69646496+rm100021@users.noreply.github.com>
2020-08-13 11:01:39 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("MY name is Rohini Murali");
+		System.out.println("I have lived in the US for 11 years and in India for 6 years");
+		System.out.println("I want to go to UCLA or UC Davis");
+		System.out.println("I love watching TV shows in my free time");
+		System.out.println("I want to major in Biology");
+	}
+}
